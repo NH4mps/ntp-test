3055aa26464dbea94f39c3fe4d564aec07d300be
NH4mps <NH4mps@github.com>
2022-10-09 22:33:32 +0300

    Заполнена функция main сервера

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -4,6 +4,20 @@
 
 int main()
 {
+    try
+    {
+        auto st = Db::InitStorage().sync_schema();
+
+        boost::asio::io_service io_service;
+
+        Server s(io_service);
+
+        io_service.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
 
     return 0;
 }
